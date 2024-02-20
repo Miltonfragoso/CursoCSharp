4f9ac9ca5dbddd90099e9aa79c9b9f5d3c0ff881
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-02-19 23:14:55 -0300

    Operadores Lógicos.

---
+++ Operadores_Logicos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Operadores_Logicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores lógicos mais utilizados são:                              
+            /* E   (&&) As 2 condições tem de ser verdadeira
+               OU  (||) Pelo menos uma ou as 2 condições tem de ser verdadeira
+               NÃO (!)
+            */
+
+            /*PRESEDÊNCIA
+             * Operador NÃO ! > && > ||
+             */
+
+            bool c1 = 4 != 5;
+            Console.WriteLine(c1);
+
+            bool c2 = 2 > 3 && 4 != 5;
+            Console.WriteLine(c2);
+
+            bool c3 = 2 > 3 || 4 != 5;
+            Console.WriteLine(c3);
+
+
+            bool c4 = !(2 > 3) && 4 != 5;
+            Console.WriteLine(c4);
+
+            Console.WriteLine("----------------------------");
+
+            bool c5 = 10 < 5;
+            bool c6 = c3 || c4 && c5;
+
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+
+        }
+    }
+}
