433821dce0eb9ea68126caa0ffbbcb2db37a0c82
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-01 00:31:18 -0300

    Exercício calcular a área de um triangulo adicionando POO. uma CLASS e MÉTODO.

---
+++ Exerc_Triangulo_METODO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_Triangulo_METODO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X = new Triangulo();
+            Triangulo Y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
+            double areaX = X.Area();
+            double areaY = Y.Area();
+
+
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Exerc_Triangulo_METODO/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exerc_Triangulo_METODO
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+        /*O método é uma função dentro da class
+         * Com o método evitamos a repetição do código
+         * Quando o método esta na mesma class não precisa passar parametro*/
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
