f31d81dc64fe4cca6b49308278ad64bb5c4a242f
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-01 00:13:53 -0300

    Ecercício 2 utilizando class.

---
+++ Exerc_Class_2/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Exerc_Class_2
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Exerc_Class_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+/* Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+   médio dos funcionários.
+*/
+
+namespace Exerc_Class_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A = new Funcionario();
+            Funcionario B = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionario A: ");
+            A.Nome = Console.ReadLine();
+            A.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os dados do funcionario B: ");
+            B.Nome = Console.ReadLine();
+            B.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Salariomedio = (A.Salario + B.Salario) / 2.0;
+            Console.WriteLine("Salario Médio = " + Salariomedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
