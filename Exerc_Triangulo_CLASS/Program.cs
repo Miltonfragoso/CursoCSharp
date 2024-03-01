37ed6ae36c290484f971248caf9d087a039a7e30
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-02-29 23:58:45 -0300

    Exercício calcular a área de um triangulo adicionando POO. uma CLASS.

---
+++ Exerc_Triangulo_CLASS/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_Triangulo_CLASS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+
+             X = new Triangulo();
+             Y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double p = (X.A + X.B + X.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - X.A) * (p - X.B) * (p - X.C));
+
+            p = (Y.A + Y.B + Y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - Y.A) * (p - Y.B) * (p - Y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Exerc_Triangulo_CLASS/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+      /*CLASS 
+       *É um tipo estruturado que pode conter(membros):
+       *Atributos(dados/campos)
+       *Métodos(funções/operações)
+       *
+       *
+       *A CLASS também pode prover muitos outros recursos, tais como:
+       *Construtores
+       *Sobrecarga
+       *Encapsulamento
+       *Herança
+       *Polimorfismo
+       *
+       *
+       *A CLASS é o fundamento principal da POO
+       */
+namespace Exerc_Triangulo_CLASS
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
