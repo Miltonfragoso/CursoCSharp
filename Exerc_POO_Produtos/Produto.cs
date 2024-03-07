f3c9e8713506b7400a4e835adab2c7d460ead900
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-06 21:26:53 -0300

    Exercício Produtos em estoque utilizando class e métodos.

---
+++ Exerc_POO_Produtos/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exerc_POO_Produtos
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            //Quantidade += quantidade;
+            Quantidade = Quantidade + quantidade;
+        }
+
+
+        public void RemoverProdutos(int quanuantidade)
+        {
+            //Quantidade -= quanuantidade;
+            Quantidade = Quantidade - quanuantidade;
+        }
+
+
+        //Toda CLASS em c# é uma subclass de Object que possui os seguintes métodos: GetType, Equals, GetHasCode e ToString que converte o objeto para string
+        //Esse método ToString foi criado para evitar a repetição de código visto que será imprsso a saída de dados por três vezes
+        // função que permite converter o objeto para string no nosso caso criando uma concatenação de string
+        // O override cria uma sobreposição da class que é uma subclass de object.
+        public override string ToString()
+        {
+            return Nome
+                   + ", $ "
+                   + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", "
+                   + Quantidade
+                   + " unidades, Valor Total em estoque:  $ "
+                   + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exerc_POO_Produtos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_POO_Produtos
+{
+    /*Fazer um programa para ler os dados de um produto em estoque(nome, preço, quantidade)
+     Em seguida mostra os dados do produto(nome, preço, quantidade no estoque, valor total no estoque).
+
+    -Adicionar mais produtos no estoque e mostra os dados novamente.
+    -retirar alguns produtos no estoque e mostra os dados do produto*/
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            // Imprimindo a saída desta forma gera repetição de código visto que faremos 3 saídas
+            //Console.WriteLine();
+            //Console.WriteLine("Dados do produto: " + p.Nome + ", $" + p.Preco + " , " + p.Quantidade);
+
+
+
+            //O método ToString da class produto é chamado implicitamente. chamamos sempre que precisarmos imprimir saída.
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionado em estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+        }
+    }
+}
