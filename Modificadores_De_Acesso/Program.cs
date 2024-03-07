b999bb005770ba6e7a8da0eec36bc5702f4b43c6
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-07 16:28:15 -0300

    Modificadores de acesso.

---
+++ Modificadores_De_Acesso/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modificadores_De_Acesso
+{
+    internal class Program
+    {
+        /*Modificadores de acesso são palavras chaves usadas para espesificar a acessibilidade de um membro ou tipo.
+         * 
+         
+         Public => permite que o membro da classe pode ser acessado por : Própia classe, subclasses no assembly, classes do assembly, subclasses fora do assembly, classes fora do assembly.
+
+         Protected internal => permite que o membro da classe pode ser acessado por : Própia classe, subclasses no assembly, classes do assembly, subclasses fora do assembly.
+
+         Internal=> permite que o membro da classe pode ser acessado por : Própia classe, subclasses no assembly, classes do assembly.
+
+         Protected => permite que o membro da classe pode ser acessado por : Própia classe, subclasses no assembly, subclasses fora do assembly.
+
+         Private Protected => permite que o membro da classe pode ser acessado por : Própia classe, subclasses no assembly.
+
+         Private => permite que o membro da classe pode ser acessado por : Própia classe.
+        */
+    }
+}
