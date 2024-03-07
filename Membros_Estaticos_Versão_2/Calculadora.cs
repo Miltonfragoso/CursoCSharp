3fab4b47f1f0bbd367c34738e86b472fdb03eef2
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-06 23:42:22 -0300

    Membors estáticos versão 2

---
+++ Membros_Estaticos_Versão_2/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Membros_Estaticos_Versão_2
+{
+    internal class Calculadora
+    {
+        public double Pi = 3.14;
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
