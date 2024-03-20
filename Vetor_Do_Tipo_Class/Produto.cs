fab81c5e0ddea00cbdd0a94f9b005fa69dbf00c3
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-03-20 01:40:29 -0300

    Vetores do tipo EXTRUTURE e do tipo CLASS.

---
+++ Vetor_Do_Tipo_Class/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetor_Do_Tipo_Class
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Vetor_Do_Tipo_Class/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor_Do_Tipo_Class
+{
+    // VETOR DO TIPO CLASS
+    /*Fazer um programa para ler um número inteiro N e os dados (nome e preço ) de N produtos. Armazene os N produtos em um vetor. 
+      Em seguida, mostrar o preço médio dos produtos.*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Produto[] vet = new Produto[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                soma = soma + vet[i].Preco;
+            }
+
+            double precoMedio = soma / N;
+            Console.WriteLine("Preço Médio = " + precoMedio.ToString(), CultureInfo.InvariantCulture);
+        }
+    }
+}
