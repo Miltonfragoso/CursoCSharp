fe2ddb918f6125d418239e0e50deff739dae242a
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-09-12 16:52:18 -0300

    Revisão.

---
+++ 29 - Exerc_Estrutura_Para(for)04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace _29___Exerc_Estrutura_Para_for_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+                segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+             */
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+                if(y == 0)
+                {
+                    Console.WriteLine("Divisão impossivel");
+                }
+                else
+                {
+                    double div = (double)x / y;
+                    Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
