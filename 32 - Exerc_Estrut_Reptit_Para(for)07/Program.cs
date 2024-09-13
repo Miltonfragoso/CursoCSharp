d735c05fd22558cf97f49e8b7beb0cf319d2dc71
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-09-12 21:14:17 -0300

    Exercício 7 estrutura repetitiva for.

---
+++ 32 - Exerc_Estrut_Reptit_Para(for)07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _32___Exerc_Estrut_Reptit_Para_for_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------------");
+
+            double quadrado = 0;
+            double cubo = 0;
+            for (int i = 1; i <= N; i++)
+            {
+
+                quadrado = (Math.Pow(i, 2));
+                cubo = (Math.Pow(i, 3));
+                Console.WriteLine("{0} {1} {2}", i, quadrado, cubo);
+            }
+
+        }
+    }
+}
