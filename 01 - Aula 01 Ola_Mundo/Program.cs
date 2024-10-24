b61e6a8c8876dba829f050238aeaf557454825da
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-10-23 22:06:25 -0300

    first commit

---
+++ 01 - Aula 01 Ola_Mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01___Aula_01_Ola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
---
+++ 02 - Comentarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02___Comentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comentário de uma única linha
+
+            /* Comentário
+             * de múltiplas
+             * linhas 
+             */
+
+            adicionar(10, 11);
+        }
+
+        //se fizermos um comentário de 3 linhas teremos a extrutura abaixo e onde usarmos a função adicionar teremos uma descrição
+
+
+        /// <summary>
+        /// Executa a adição de valores a e  b 
+        /// </summary>
+        /// <param name="a">Parcela 1</param>
+        /// <param name="b">Parcela 2</param>
+        static void adicionar(int a, int b)
+        {
+
+        }
+    }
+}
+
