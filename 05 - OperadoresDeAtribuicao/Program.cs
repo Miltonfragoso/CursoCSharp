a0be3d0fa290658ecc3cc2397c4ecd8d55b7610f
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-10-24 15:36:06 -0300

    Aula 05 Operadores de Atribuição.

---
+++ 05 - OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05___OperadoresDeAtribuicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             //Operador de atribuição (=)
+            //Operador de atribuição comulativa
+            //(+=, -=, *=, /=, %=)
+
+            // a recebe 10 
+            int a = 10;
+            Console.WriteLine(a);
+
+            // 10 + 2
+            a += 2;
+            Console.WriteLine(a);
+
+            a -= 2;
+            Console.WriteLine(a);
+
+            a *= 2;
+            Console.WriteLine(a);
+
+            a /= 2;
+            Console.WriteLine(a);
+
+            a %= 2;
+            Console.WriteLine(a);
+
+
+            //Operador aritmético / atribuição(++; --)
+            int b = 10;
+            Console.WriteLine(b);
+
+            b++;
+            Console.WriteLine(b);
+
+            int c = 10;
+            Console.WriteLine(c);
+
+            c--;
+            Console.WriteLine(c);
+        }
+    }
+}
