7c7817a4c6683c57703c506e300dcccc8143da25
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-10-24 22:39:53 -0300

    Aula 08 Entrada de dados.

---
+++ 08 - EntradaDeDados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.ReadLine(); Lê da entrada padrão (teclado) até a quebra de linha (Enter)
+             Retorna os dadosn lidos na forma de string*/
+
+            string frase = Console.ReadLine();
+
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+
+            /*Escrever uma frase e armazenar cada palavra em uma variável
+             usamos o comando split('') que é uma função que serve para recortar uma string*/
+
+            string[] V = Console.ReadLine().Split(' ');
+            string a = V[0];
+            string b = V[1];
+            string c = V[2];
+
+            Console.WriteLine("você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
