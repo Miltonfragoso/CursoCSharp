f054a58d50d7ccbfb6db7b173b61c371702288fc
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-10-28 21:40:19 -0300

    Aula  11 Exercícios estrutura sequencial.

---
+++ 11 - ExercEstruSequenc01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercEstruSequenc01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+               mensagem explicativa, conforme exemplos.
+             */
+
+            Console.WriteLine("Entrada:");
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+
+            int soma = N1 + N2;
+            Console.WriteLine();
+            Console.WriteLine("Saída");
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
