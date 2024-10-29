00f88c1a6929a656fe430a2e2a14a06bcbab2d22
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2024-10-28 21:57:03 -0300

    Aula 12 Exercício estrutura sequencial 02.

---
+++ 12 - ExercEstruSequenc02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace _12___ExercEstruSequenc02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+              casas decimais conforme exemplos.
+              Fórmula da área: area = π . raio2
+              Considere o valor de π = 3.14159*/
+
+            Console.WriteLine("Entrada:");
+            double N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = 3.14159;
+
+            double Area = Math.Pow(N1, 2.0) * p;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Saída:");
+            Console.WriteLine("A=" + Area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
