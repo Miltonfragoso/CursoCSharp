d326795a296c2434ccd83848001f0b9323fc5559
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-04 22:10:39 -0300

    Aula 29 Exercício Estrtutura Para(For)04

---
+++ 29 - ExercicioEstrtuturaPara(For)04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _29___ExercicioEstrtuturaPara_For_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+                segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+             */
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+                if (y == 0)
+                {
+                    Console.WriteLine("Divisão impossivel");
+                }
+                else
+                {
+                    double div = (double)x / y;
+                    Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
