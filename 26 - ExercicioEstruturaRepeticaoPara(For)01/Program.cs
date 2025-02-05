0a9a1c8c5996890e59325a3062211a017d208b87
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-04 21:56:12 -0300

    Aula 26 Exercício Estrtutura Para(For)01.

---
+++ 26 - ExercicioEstruturaRepeticaoPara(For)01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _26___ExercicioEstruturaRepeticaoPara_For_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+              X, se for o caso.
+             */
+
+            Console.WriteLine("Entrada:");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
