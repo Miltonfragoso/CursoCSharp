662caa28f8ce6dcfefa0e916e757b00aad18006c
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-04 22:19:11 -0300

    Aula 31 Exercício Estrtutura Para(For)06

---
+++ 31 - ExercicioEstrtuturaPara(For)06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _31___ExercicioEstrtuturaPara_For_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ler um número inteiro N e calcular todos os seus divisores.*/
+
+            Console.WriteLine("Entrada: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
