eb9c5f1c0d063df0d4f0dc30eb367f40fd041f7a
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-05 21:40:35 -0300

    Aula 36 Exercícios com classes e métodos.

---
+++ 36 - ExercíciosPOO01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _36___ExercíciosPOO01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Entre com a largura e a altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Area = " + retangulo.Area().ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Perímetro = " + retangulo.Perimetro().ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Diagonal = " + retangulo.Diagonal().ToString("F2"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
