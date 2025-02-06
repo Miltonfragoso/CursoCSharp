8fafef9ceec4df4f4b130cdf2ae30b67932d888e
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-06 17:04:49 -0300

    Aula 41 membros estaticos parte 03 onde as funções da classe podem ser acessadas sem instancia a classe.

---
+++ 41 - MembrosEstaticosParte03/Program.cs
@@ -0,0 +1,27 @@
+﻿/*A grande questão nos membros estaticos são os atributos da classe que tem os valores fixos
+ * os valores dos atributos não mudam.
+ * Uma classe cujo os atributos tem valores fixos não precisamos instanciar atraves da classe podemos
+ * chamar direto suas funções.
+ */
+using System;
+using System.Globalization;
+
+namespace MembrosEstaticosParte03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
