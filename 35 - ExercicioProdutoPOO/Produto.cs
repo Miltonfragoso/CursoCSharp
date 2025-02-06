14487ea370bafd470c0a04ac9075eeafa9413de5
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-05 21:12:31 -0300

    Aula 35 exercício produtos com classe métodos e sobreposição de método.

---
+++ 35 - ExercicioProdutoPOO/Produto.cs
@@ -0,0 +1,58 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+  Toda classe em c# é uma sublasse da classe Object
+  A classe Obect possui os seguintes métodos:
+  *GetType - retorna o tipo do objeto
+  * Equals - compara se o objeto é igual a outro
+  * GetHasCode - retorna um código hash do objeto
+  * Tostring - converte o objeto ou a classe nesse caso Produto para uma string
+   Ao implementarmos o método Tostring em nossa classe podemos definir como esse objeto é representao na forma de string
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioProdutoPOO
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        /*Variável de entrada de função sempre com letra minuscula
+         este método tem como função receber um valor e soma-lo a quantidade*/
+        public void AdicionarProdutos(int quatidade)
+        {
+            Quantidade = Quantidade + quatidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+
+        /*Implementação do método Tostring, por ser método de outra classe precisamos fazer uma sobreposição
+         a palavra override indica uma sobreposição
+        MÉTODO PARA PERSONALIZARA SAÍDA DE DADOS*/
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ 35 - ExercicioProdutoPOO/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Fazer um programa para ler os dados de um produto em estoque(nome, preço e quantidade no estoque)
+ em seguida, mostrar os dados do produto(nome, preço quantidade no estoque, valor total no estoque)
+ realizar uma entrada no estoque e mostrar novamente os dados do produto
+ Realizar uma saída no estoque e mostrar novamente os dados do produto
+                                      PRODUTO
+                  ------------------------------------------
+                        -Nome: String
+                        -Preço: double
+                        -Quantidade: int
+                  -------------------------------------------
+                        + ValorTotalEm Estoque(): double
+                        + AdicionarProduto(quantity: int): void
+                        +RemoverProdutos(quantity: int): void
+*/
+using System;
+using System.Globalization;
+
+namespace ExercicioProdutoPOO
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p.ToString());
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int Qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(Qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p.ToString());
+
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            Qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(Qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p.ToString());
+        }
+    }
+}
