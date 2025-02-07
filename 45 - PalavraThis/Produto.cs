e712b06557d09c2941e62f9b282e1c63f85967ca
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-07 01:14:20 -0300

    Aula 45 Palavra this.

---
+++ 45 - PalavraThis/Produto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace PalavraThis
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+
+
+        //Sobrecarga e reaproveitando o construtor padrão no construtor de 2 argumentos usando o : this 
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        //Sobrecarga e reaproveitando o construtor de 2 argumentos no construtor de 3 argumentos usando o : this 
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+
+        }
+
+
+        public void RemoverProdutos(int quanuantidade)
+        {
+            Quantidade -= quanuantidade;
+
+        }
+
+
+        //Toda CLASS em c# é uma subclass de Object que possui os seguintes métodos: GetType, Equals, GetHasCode e ToString que converte o objeto para string
+        //Esse método ToString foi criado para evitar a repetição de código visto que será imprsso a saída de dados por três vezes
+        // função que permite converter o objeto para string no nosso caso criando uma concatenação de string
+        // O override cria uma sobreposição da class que é uma subclass de object.
+        public override string ToString()
+        {
+            return Nome
+                   + ", $ "
+                   + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", "
+                   + Quantidade
+                   + " unidades, Valor Total em estoque:  $ "
+                   + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 45 - PalavraThis/Program.cs
@@ -0,0 +1,72 @@
+﻿/*Palavra this
+• É uma referência para o próprio objeto
+
+• Usos comuns:
+• Diferenciar atributos de variáveis locais (Java)
+• Referenciar outro construtor em um construtor
+• Passar o próprio objeto como argumento na chamada de um método ou construtor*/
+
+using System;
+using System.Globalization;
+
+namespace PalavraThis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.Write("Quantidade no estoque: ");
+            //int quantidade = int.Parse(Console.ReadLine());
+
+
+            //usando o construtor de 2 argumentos
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionado em estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+
+            /*Sintaxe alternativa para inicialização de valores sem a construção do construtor permitido no c#
+             Produto p  = new Produto () {
+                Nome = "TV"
+                Preco = 900.00
+                Quantidade = 0 
+            }
+
+            o objeto recebe os valores direto ao inicializar
+             */
+        }
+    }
+}
+
+
+/*  Diferenciar atributos de variáveis locais
+ *  public Produto(string Nome, double Preco) {
+           this.Nome = Nome;
+           this.Preco = Preco;
+           Quantidade = 0;
+    }*/
