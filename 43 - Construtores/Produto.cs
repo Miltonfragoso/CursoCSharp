cd3bcb77e89e31a986aa15fd3733f8c37b03ab05
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-07 00:34:46 -0300

    Aula 43 Construtores.

---
+++ 43 - Construtores/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Construtores
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        /*Este construtor vai obrigar que ao instanciar a classe Produto os valores não podem ser nulos
+         terão que ser passados assim que instanciar a classe.*/
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+
+        }
+
+
+        public void RemoverProdutos(int quanuantidade)
+        {
+            Quantidade -= quanuantidade;
+
+        }
+
+
+        //Toda CLASS em c# é uma subclass de Object que possui os seguintes métodos: GetType, Equals, GetHasCode e ToString que converte o objeto para string
+        //Esse método ToString foi criado para evitar a repetição de código visto que será imprsso a saída de dados por três vezes
+        // função que permite converter o objeto para string no nosso caso criando uma concatenação de string
+        // O override cria uma sobreposição da class que é uma subclass de object.
+        public override string ToString()
+        {
+            return Nome
+                   + ", $ "
+                   + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", "
+                   + Quantidade
+                   + " unidades, Valor Total em estoque:  $ "
+                   + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 43 - Construtores/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Construtor
+• É uma operação especial da classe, que executa no momento da
+instanciação do objeto
+• Usos comuns:
+• Iniciar valores dos atributos
+• Permitir ou obrigar que o objeto receba dados / dependências no momento de sua
+instanciação (injeção de dependência)
+• Se um construtor customizado não for especificado, a classe disponibiliza o
+construtor padrão:
+• Produto p = new Produto();
+• É possível especificar mais de um construtor na mesma classe (sobrecarga)*/
+
+using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionado em estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+        }
+    }
+}
