e3651342779824154312291fed18ffb619a2226b
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-09 22:45:29 -0300

    Aula 46 Emcapsulamento.

---
+++ 46 - Encapsulamento/Produto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        // implementação de uma propiedade
+        // value é o nome que vem do parametro
+        public string Nome
+        {
+            get { 
+                return _nome; 
+            }
+
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { 
+                return _preco; 
+            }
+            set { 
+                _preco = value;
+            }
+        }
+
+        public int Quantidade
+        {
+            get { 
+                return _quantidade;
+            }
+            set { 
+                _quantidade = value; 
+            }
+        }
+
+
+        /* Auto propriedade
+         * 
+         * private string _nome;
+           private double _preco {get; private set:};
+           private int _quantidade {get; private set;};*/
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 46 - Encapsulamento/Program.cs
@@ -0,0 +1,56 @@
+﻿/*• EMCAPSULAMENTO PRIMEIRO PILAR DA ORIENTAÇÃO A OBJETO
+ * é um princípio que consiste em
+esconder detalhes de implementação
+de um componente, expondo apenas
+operações seguras e que o mantenha
+em um estado consistente.
+• Regra de ouro: o objeto deve sempre
+estar em um estado consistente, e a
+própria classe deve garantir isso.*/
+
+/* Opção 1: implementação manual
+• Todo atributo é definido como private
+• Implementa-se métodos Get e Set para cada atributo, conforme
+regras de negócio
+• Nota: não é usual na plataforma C#*/
+
+using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
