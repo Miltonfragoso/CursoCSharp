556290732735ba60c08b2dd6218349ec75edd24b
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-09 23:14:16 -0300

    Aula 47 - Propriedades e auto propriedades.

---
+++ 47 - PropiedadeE Autopropriedades/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Uma propriedade é um membro que oferece um mecanismo flexível para ler,
+gravar ou calcular o valor de um campo particular. As propriedades podem ser
+usadas como se fossem atributos públicos, mas na verdade elas são métodos
+especiais chamados "acessadores". Isso permite que os dados sejam
+acessados facilmente e ainda ajuda a promover a segurança e a flexibilidade
+dos métodos.
+
+
+AUTO PROPERTIES
+É uma forma simplificada de se declarar propriedades que não
+necessitam lógicas particulares para as operações get e set.
+*/
+using System;
+
+namespace _47___PropiedadeE_Autopropriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
