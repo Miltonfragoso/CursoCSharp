ad18cc13b518337ed8f9b8908f63b32c96fbe952
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-20 12:39:24 -0300

    Aula 57 Modificadores de acesso REF e OUT

---
+++ 57 - ModificadorDePrarametrosREFeOUT/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorDePrarametrosREFeOUT
+{
+    internal class Calculadora
+    {
+        //Uma função para triplicar o valor de um número
+        public static void Triplicar(int x)
+        {
+            x = x * 3; 
+        }
+
+        /*Neste exemplo a função recebe dois parametros, um que é o valor a ser triplicado o outro uma variável para armazenar o resultado*/
+        public  static void Triplicar2(int origem, out int triplicar)
+        {
+            triplicar = origem * 3;
+        }
+    }
+}
---
+++ 57 - ModificadorDePrarametrosREFeOUT/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Modificador de parametros REF: ele ajuda nas funções cujo o retorno é um Void
+ *Se não usarmos o REF tanto na função quanto na sua chamada ele não vai alterar o valor da variável na memoria  Steak 
+ *somente na função que esta  na Head o REF vai apontar o resultado da função para a variável na stack como uma referencia
+ */
+
+
+/*Modificador de parametros OUT: ele ajuda nas funções cujo o retorno é um Void
+ *Se não usarmos o OUT tanto na função quanto na sua chamada ele não vai alterar o valor da variável na memoria  Steak 
+ *somente na função que esta  na Head o OUT vai apontar o resultado da função para a variável na stack como uma referencia
+ *O OUT NÃO EXIGE QUE A VARIÁVEL IRIGINAL OU NA STACK SEJA INICIADA OUT É UMA VARIÁVEL DE SAÍDA
+ */
+
+
+using System;
+
+namespace ModificadorDePrarametrosREFeOUT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculadora.Triplicar(a);
+
+            Console.WriteLine(a);
+
+
+            int b = 10;
+            int resultado;
+
+            Calculadora.Triplicar2(b, out resultado);
+            Console.WriteLine(resultado);
+        }
+    }
+}
