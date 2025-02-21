7c0df39dc25afe6f26ee86becb216d6eb35b43f8
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-02-20 22:34:18 -0300

    Aula 60 Listas.

---
+++ 60 - Listas/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*LISTAS: contem dados do mesmo tipo, os elementos são acessados por meio de posições, inícia vazia e seus elementos são alocados sob demanda,
+ cada elemento ocupa um nó ou nodo da lista.
+*
+*Classe List: utilizamos a lista com a class list que pertence ao namespace System.colletions.generic;
+*
+*Vantagens: Tamanho variável, começa vazia e vamos adicionando elementos, facilidade de inserir e remover elementos
+*Desvantagens: acesso sequencial e não direito a posição do elemento
+*/
+using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<String>();
+
+            List<String> list2 = new List<String> { " Milton ", " Danilsa ", " Yahminah "};
+        }
+    }
+}
