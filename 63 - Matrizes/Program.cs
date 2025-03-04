d89daa67bbdf790a3e6ff739d23c9becbd584903
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-03-04 13:53:42 -0300

    Aula 63 - Matrizes.

---
+++ 63 - Matrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Em programação "Matriz" é o nome dado a arranjos bidimensionais (Linhas e Colunas)
+ *A matriz também possui dados homogênios do mesmo tipo
+ *elementos acessados por meio de posições
+ *quando instanciada a matriz, precisamos falar o seu tamanho igual a vetores
+ */
+using System;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = new double[2, 3];
+
+            //Função Length: total de linhas e colunas
+            Console.WriteLine(matriz.Length);
+
+            //Função Rank: total de linhas
+            Console.WriteLine(matriz.Rank);
+        }
+    }
+}
