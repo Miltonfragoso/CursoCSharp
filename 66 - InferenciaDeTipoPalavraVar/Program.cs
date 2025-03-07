f057cc8c17465ada2604bd175e62969061dd3226
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-03-06 23:52:10 -0300

    Aula 66 Inferência de tipos. palavra var.

---
+++ 66 - InferenciaDeTipoPalavraVar/Program.cs
@@ -0,0 +1,22 @@
+﻿/*A variável var recebe o tipo conforme o valor atribuido inferindo nela o tipo string bool ou int.
+ */
+
+using System;
+
+namespace InferenciaDeTipoPalavraVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Milton";
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
