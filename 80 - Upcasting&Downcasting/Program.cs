904ccefdc0bb34ea38e74a3438b94fcec3a3b663
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-05-01 12:37:57 -0300

    Upcasting  & Downcasting

---
+++ 80 - Upcasting&Downcasting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _80___Upcasting_Downcasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
