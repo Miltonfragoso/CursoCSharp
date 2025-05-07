07c18e6287491c8ec9db27f04f8223fa4458e455
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-05-07 06:15:48 -0300

    Aula 81 - Sobrescreita Sopreposição.

---
+++ 81 - SobreposicaoSobrescrita/Entities/ContaPoupanca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SobreposicaoSobrescrita.Entities
+{
+    internal class ContaPoupanca : Conta
+    {
+        public double TaxaJuros { get; set; }
+
+        public ContaPoupanca()
+        {
+        }
+
+        public ContaPoupanca(int numero, string titular, double saldo, double taxaJuros) : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+
+        public void AtualizarSaldo()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
+
+        //sobrescrevendo o método saque
+        public override void Saque(double quantia)
+        {
+            Saldo -= (quantia);
+        }
+    }
+}
---
+++ 81 - SobreposicaoSobrescrita/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Sobreposição ou sobrescrita é a implementação de um método de uma superclasse na subclasse
+ * 
+ * Para que um método comum (Não abstrato) possa ser sobreposto, deve ser incluído nele o prefico "VIRTUAL"
+ * 
+ * Ao sobrescrever um método, devemos incluir nele o prefixo "override"
+ */
+
+
+using SobreposicaoSobrescrita.Entities;
+using System;
+
+namespace SobreposicaoSobrescrita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta acc1 = new Conta(1001, "Alex", 500.0);
+            Conta acc2 = new ContaPoupanca(1002, "Ana", 500.0, 0.01);
+
+            acc1.Saque(10.0);
+            acc2.Saque(10.0);
+
+            Console.WriteLine(acc1.Saldo);
+            Console.WriteLine(acc2.Saldo);
+        }
+    }
+}
