4a884b2ee31ed85d9b30bfbc733656f2aea7c8b6
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-08-04 14:01:02 -0300

    Aula 96 bloco using trabalhando com arquivos.

---
+++ 96 - Bloco_using/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Using block: é uma sintaxie simplificada que garante que os objetos IDisposable serão fechados.
+ * Objetos IDisposable Não são gerenciados pelo CLR.Eles precisam ser manualmente fechados.
+ * EX: Font, FileStream, StreamReader, StreamWriter
+ */
+using System;
+using System.IO;
+
+namespace Bloco_using
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\MILTON\Desktop\Arquivos baixados\File1.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
