e02fadcbcd59a4537654b59c2a647757a6e1bc46
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-08-04 19:38:36 -0300

    Aula 97 StreamWriter, trabalhando com arquivos.

---
+++ 97 - StreamWriter/Program.cs
@@ -0,0 +1,37 @@
+﻿/*StreamWriter: é um objeto instanciado apartir do FileStream.
+ * E uma stream capaz de escrever caracteres a partir de uma stream binária(ex: FileStream).
+ */
+
+
+using System;
+using System.IO;
+
+namespace Stream_Writer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //objetivo: Ler os dados do arquivo 1 e copiar no arquivo 2 porém tudo maiúsculo
+            string caminhoPastaOrigem = @"C:\Users\MILTON\Desktop\Arquivos baixados\File1.txt";
+            string caminhoPastaDestino = @"C:\Users\MILTON\Desktop\Arquivos baixados\File2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(caminhoPastaOrigem);
+
+                using (StreamWriter sw = File.AppendText(caminhoPastaDestino))
+                {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
