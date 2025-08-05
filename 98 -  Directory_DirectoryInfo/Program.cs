b0ce4dcf4bba0bf7a497087f0d53c20509166a5e
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-08-04 23:25:07 -0300

    Aula 98 classes Directory DirectoryInfo, trabalhando com arquivos.

---
+++ 98 -  Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Directory e DirectoryInfo são duas classes servem para nos fornecer operações com pastas
+ * ex: criar pasta,listar arquivos etc
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Directory_DirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\MILTON\Desktop\Arquivos baixados";
+
+            try
+            {
+                //Listar todas as pastas apartir da pasta Arquivos baixados
+                IEnumerable<string> pastas = Directory.EnumerateDirectories(path,"*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Pastas: ");
+                foreach (string pasta in pastas) {
+                    Console.WriteLine(pasta);
+                }
+
+
+
+                //Listar todos os arquivos apartir da pasta Arquivos baixados
+                IEnumerable<string> arquivos = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Arquivos: ");
+                foreach (string pasta in arquivos)
+                {
+                    Console.WriteLine(pasta);
+                }
+
+
+                //Criar uma pasta
+                Directory.CreateDirectory(path + @"\nwefolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
