e04423cbd7a94b7673d78204b74a76593111829c
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-04 11:44:04 -0300

    Aula 106 Introdução oas generics.

---
+++ 106 - IntroducaoGenerics/ImpressaoService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroducaoGenerics
+{
+    public class ImpressaoService
+    {
+        private int[] _valores = new int[10];
+
+        private int _contador = 0;
+
+        public void AddValor(int value)
+        {
+            if (_contador == 10)
+            {
+                throw new InvalidOperationException("Limite de valores atingido");
+            }
+
+            _valores[_contador] = value;
+            _contador++;
+
+        }
+
+        public int Primeiro()
+        {
+            if (_contador == 0)
+            {
+                throw new InvalidOperationException("Nenhum valor adicionado");
+            }
+            return _valores[0];
+        }
+
+        public void Imprimir()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _contador -1; i++)
+            {
+                Console.Write(_valores[i] + ", ");
+            }
+            if (_contador > 0)
+            {
+                Console.Write(_valores[_contador - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ 106 - IntroducaoGenerics/Program.cs
@@ -0,0 +1,45 @@
+﻿/* PROBLEMA: PARA DEMOSTRAR O REUSO DE CÓDIGO COM GENERICS
+ * Deseja-se fazer um programa que leia um conjunto de N números inteiros (N de
+    1 a 10), e depois imprima esses números de forma organizada conforme
+    exemplo. Em seguida, informar qual foi o primeiro valor informado.
+
+How many values? 3 de impressão:
+10
+8
+23
+[10, 8, 23]
+First: 10
+
+Criar um serviço de impressão
+
++ addValor(value : int) void
++primeiro() int
++ imprimir() void
+
+
+ */
+using System;
+
+namespace IntroducaoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImpressaoService impressaoService = new ImpressaoService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                impressaoService.AddValor(value);
+            }
+
+            impressaoService.Imprimir();
+
+            Console.WriteLine("Primeiro: " + impressaoService.Primeiro());
+        }
+    }
+}
