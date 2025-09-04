02cff31409527e94806d32c4f6a4a173d11d8959
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-04 14:59:41 -0300

    Aula 107 resolvendo um exercício usando um serviço genérico com restrição de uma interface IComparable

---
+++ 107 - ExercRestricoesGenerics/Entities/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercRestricoesGenerics.Entities
+{
+    public  class Produto : IComparable
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Produto))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+            Produto other = obj as Produto;
+            return Preco.CompareTo(other.Preco);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 107 - ExercRestricoesGenerics/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Problema
+ * Uma empresa de consultoria deseja avaliar a performance de produtos,
+funcionários, dentre outras coisas. Um dos cálculos que ela precisa é encontrar
+o maior dentre um conjunto de elementos. Fazer um programa que leia um
+conjunto de N produtos, conforme exemplo, e depois mostre o mais caro deles.
+
+ Enter N: 3                                                             Criar um serviço de cálculo:
+Computer,890.50                                                              + max<T>(List<T> list) T 
+IPhone X,910.00
+Tablet,550.00
+Max:
+IPhone, 910.00
+
+ */
+
+using ExercRestricoesGenerics.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ExercRestricoesGenerics.Services;
+
+namespace ExercRestricoesGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> list = new List<Produto>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Produto(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Produto p = calculationService.Max(list); // <Product> is optional
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(p);
+        }
+    }
+}
