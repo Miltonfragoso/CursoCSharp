853ec581941a0800d4f0b1c47faf5789c81d83e0
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-05 00:29:08 -0300

    Aula 110 Dictionary & SortedDictionary

---
+++ 110 - DictionarySortedDictionary/Definicao.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Dictionary<TKey, TValue>
+• É uma coleção de pares chave / valor
+• Não admite repetições do objeto chave
+• Os elementos são indexados pelo objeto chave (não possuem posição)
+• Acesso, inserção e remoção de elementos são rápidos
+• Uso comum: cookies, local storage, qualquer modelo chave-valor
+
+
+Diferenças
+• Dictionary
+• Armazenamento em tabela hash
+• Extremamente rápido: inserção, remoção e busca O(1)
+• A ordem dos elementos não é garantida
+
+• SortedDictionary
+• Armazenamento em árvore
+• Rápido: inserção, remoção e busca O(log(n))
+• Os elementos são
+
+Alguns métodos importantes
+• dictionary[key] - acessa o elemento pela chave informada
+• Add(key, value) - adiciona elemento (exceção em caso de repetição)
+• Clear() - esvazia a coleção
+• Count - quantidade de elementos
+• ContainsKey(key) - verifica se a dada chave existe
+• ContainsValue(value) - verifica se o dado valor existe
+• Remove(key) - remove um elemento pela chave
+
+ */
---
+++ 110 - DictionarySortedDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿//Neste exemplo vai ser criado um cookies
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace DictionarySortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            //Adicionando elementos ao dictionary
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99771122";
+            cookies["phone"] = "99771133";
+
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+            Console.WriteLine("Phone number: " + cookies["phone"]);
+
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is not 'email' key");
+            }
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
