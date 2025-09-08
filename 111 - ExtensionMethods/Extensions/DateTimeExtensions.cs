0f19f1c60bc7250d114af41b5db2f4fb17452ecd
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-08 15:48:43 -0300

    Aula 111 Métodos de extensão

---
+++ 111 - ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿/*Vamos criar um extension method chamado "ElapsedTime()" no struct
+DateTime para apresentar um objeto DateTime na forma de tempo
+decorrido, podendo ser em horas (se menor que 24h) ou em dias caso
+contrário. Por exemplo:
+
+
+o parametro thisObj é uma referência para o próprio objeto DateTime que esta sendo estendido
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ExtensionMethods.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ 111 - ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Vamos criar um extension method chamado "Cut(int)" na classe String
+    para receber um valor inteiro como parâmetro e gerar um recorte do
+    string original daquele tamanho. Por exemplo:
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Extensions
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
