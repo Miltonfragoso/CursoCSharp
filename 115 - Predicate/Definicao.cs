52d204479035d70b78b20bed9e71aae57f2a9ef8
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-09 11:39:36 -0300

    Aula 115 Predicate.

---
+++ 115 - Predicate/Definicao.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Predicate - Definição: É um delegate que representa um método que recebe um parâmetro de entrada e retorna um valor booleano (true ou false).
+ * public delegate bool Predicate<in T>(T obj);
+ * 
+ * Em C#, Predicate = um delegado que retorna bool.
+
+    Usado para verificar condições sobre objetos.
+
+    Muito usado em List<T> (Find, Exists, RemoveAll, etc.).
+
+    É basicamente o mesmo que Func<T, bool>
+
+    Usando em listas (List<T>)
+
+    A classe List<T> tem vários métodos que usam Predicate<T>:
+
+    Find(Predicate<T>) → retorna o primeiro elemento que satisfaz a condição
+
+    FindAll(Predicate<T>) → retorna todos os elementos que satisfazem a condição
+
+    Exists(Predicate<T>) → retorna true/false se algum satisfaz
+
+    RemoveAll(Predicate<T>) → remove todos que satisfazem
+
+    TrueForAll(Predicate<T>) → retorna true se todos satisfazem
+ */
---
+++ 115 - Predicate/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Fazer um programa que, a partir de uma lista de produtos, remova da
+lista somente aqueles cujo preço mínimo seja 100.
+ */
+
+using Predicate.Entities;
+using System.Collections.Generic;
+
+namespace Predicate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> list = new List<Produto>();
+
+            list.Add(new Produto("Tv", 900.00));
+            list.Add(new Produto("Mouse", 50.00));
+            list.Add(new Produto("Tablet", 350.50));
+            list.Add(new Produto("HD Case", 80.90));
+
+            //Podemos usar uma expressão lambda para definir a condição de remoção desde que atenda a assinatura do Predicate que é:
+            //receber um objeto como paramêtro e retornar um booleano.
+            list.RemoveAll(p => p.Preco >= 100.0);
+
+            list.RemoveAll(TestarProduto);
+
+            //Foreach para mostrar os produtos restantes na lista
+            foreach (Produto p in list)
+            {
+                System.Console.WriteLine(p);
+            }
+        }
+
+        //Outra forma de fazer é criar um método estático que atenda a assinatura do Predicate e passar esse método como argumento do RemoveAll
+        //Passando o método como argumento no RemoveAll
+        public static bool TestarProduto(Produto p)
+        {
+            return p.Preco >= 100.0;
+        }
+    }
+}
