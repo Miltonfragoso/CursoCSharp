631bb1eb6ef91da81e81a710cfb56b2b766636aa
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-10 10:21:24 -0300

    Aula 118 Introdução ao LINQ

---
+++ 118 - IntroducaoLinq/Definicao.cs
@@ -0,0 +1,15 @@
+﻿/*LINQ - Language Integrated Query
+    • É um conjunto de tecnologias baseadas na integração de funcionalidades de
+    consulta diretamente na linguagem C#
+    • Operações chamadas diretamente a partir das coleções
+    • Consultas são objetos de primeira classe
+    • Suporte do compilador e IntelliSense da IDE
+    • Namespace: System.Linq
+    • Possui diversas operações de consulta, cujos parâmetros tipicamente são
+    expressões lambda ou expressões de sintaxe similar à SQL
+
+    Para trabalhar com linq três passos são necessários:
+        1. DATA SOURCE (fonte de dados): array, coleção, banco de dados, XML, etc.
+        2. DEFINIR A CONSULTA (query): o que queremos consultar escrevendo a consulta com LINQ
+        3. eXECUTAR A CONSULTA (execução): utilizando um loop ou convertendo para uma coleção
+ */
---
+++ 118 - IntroducaoLinq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace IntroducaoLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data source
+            int[] numeros = new int[] { 2, 3, 4, 5 };
+
+            //Definir a consulta (query)
+            //var resultado = from n in numeros where n > 3 select n;
+            var resultado = numeros.Where(n => n > 3);
+
+            //EXECUTAR A CONSULTA
+            foreach (int n in resultado)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            Console.WriteLine("------------------------------------------");
+
+            //Data source
+            int[] numbers = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            //Definir a consulta (query)
+            var result = numeros.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //EXECUTAR A CONSULTA
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
