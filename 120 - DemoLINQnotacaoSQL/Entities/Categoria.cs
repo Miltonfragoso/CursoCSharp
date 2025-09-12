aa289dedd7f7d5f223ddfb44f22dacc57f1058a0
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-09-11 22:17:21 -0300

    Aula 120 demostração de LINQ com notação similar à SQL

---
+++ 120 - DemoLINQnotacaoSQL/Entities/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLINQnotacaoSQL.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Nivel { get; set; }
+    }
+}
--- 120 - DemoLINQnotacaoSQL/Program.cs
+++ 120 - DemoLINQnotacaoSQL/Program.cs
@@ -1,9 +1,9 @@
-﻿using DemoLinqLambla.Entities;
+﻿using DemoLINQnotacaoSQL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DemoLinqLambla
+namespace DemoLINQnotacaoSQL
 {
     public class Program
     {
@@ -41,31 +41,49 @@ namespace DemoLinqLambla
                 new Produto() { Id = 14, Nome = "Headset", Preco = 350.0, Categoria = c2 }
             };
 
-            var r1 = produtos.Where(p => p.Categoria.Nivel == 1 && p.Preco < 900.0);
+            var r1 = from p in produtos
+                     where p.Categoria.Nivel == 1 && p.Preco < 900.0
+                     select p;
             Print("Mostrar todos produtos de nível 1 e preço < 900:", r1);
 
 
-            var r2 = produtos.Where(p => p.Categoria.Nome == "Ferramentas").Select(p => p.Nome);
+            var r2 = from p in produtos
+                     where p.Categoria.Nome == "Ferramentas"
+                     select p.Nome;
             Print("Mostrar os produtos da categoria ferramentas", r2);
 
             // Criação de uma projeção anonimo que não esta declarada em nenhuma classe
-            var r3 = produtos.Where(p => p.Nome[0] == 'C').Select(p => new { p.Nome, p.Preco, CategoriaNome = p.Categoria.Nome });
+            //var r3 = produtos.Where(p => p.Nome[0] == 'C').Select(p => new { p.Nome, p.Preco, CategoriaNome = p.Categoria.Nome });
+            var r3 = from p in produtos
+                     where p.Nome[0] == 'C'
+                     select new { p.Nome, p.Preco, CategoriaNome = p.Categoria.Nome };
             Print("Produtos que começam com a letra 'C' e projeção anonima", r3);
 
 
-            var r4 = produtos.Where(p => p.Categoria.Nivel == 1).OrderBy(p => p.Preco).ThenBy(p => p.Nome);
+            var r4 = from p in produtos
+                     where p.Categoria.Nivel == 1
+                     orderby p.Preco, p.Nome
+                     select p;
             Print("Produtos de nível 1 ordenados por preço e depois por nome", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in produtos
+                      where p.Categoria.Nivel == 1
+                      orderby p.Preco, p.Nome
+                      select p).Skip(2).Take(4);
+
             Print("Pular os 2 primeiros e mostrar os 4 próximos", r5);
 
-            var r6 = produtos.FirstOrDefault();
+            //var r6 = produtos.FirstOrDefault();
+            var r6 = (from p in produtos select p).FirstOrDefault();
             Console.WriteLine("Primeiro produto da coleção (ou nulo):" + r6);
 
-            var r7 = produtos.Where(p => p.Preco > 3000.0).FirstOrDefault();
+            //var r7 = produtos.Where(p => p.Preco > 3000.0).FirstOrDefault();
+            var r7 = (from p in produtos where p.Preco > 3000.0 select p).FirstOrDefault();
             Console.WriteLine("Primeiro produto com preço > 3000 (ou nulo):" + r7);
 
-            var r8 = produtos.Where(p => p.Id == 3).SingleOrDefault();
+            //var r8 = produtos.Where(p => p.Id == 3).SingleOrDefault();
+            var r8 = (from p in produtos where p.Id == 3 select p).SingleOrDefault();
             Console.WriteLine("Produto com Id = 3 (ou nulo):" + r8);
 
             var r9 = produtos.Max(p => p.Preco);
@@ -83,7 +101,8 @@ namespace DemoLinqLambla
             var r13 = produtos.Where(p => p.Categoria.Id == 1).Select(p => p.Preco).Aggregate(0.0, (x, y) => x + y);
             Console.WriteLine("Soma usando Aggregate: " + r13);
 
-            var r14 = produtos.GroupBy(p => p.Categoria);
+            //var r14 = produtos.GroupBy(p => p.Categoria);
+            var r14 = from p in produtos group p by p.Categoria;
             foreach (IGrouping<Categoria, Produto> grupo in r14)
             {
                 Console.WriteLine("Categoria " + grupo.Key.Nome + ":");
@@ -98,3 +117,4 @@ namespace DemoLinqLambla
         }
     }
 }
+
